29f68cf20d547f03919e55a96e89467eed61f81f
phuongkhpt <anhyeuemnhieu57@gmail.com>
2024-10-29 20:38:38 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -7,6 +7,6 @@ temp=$(echo $response | jq -r '.main.temp')
 humidity=$(echo $response | jq -r '.main.humidity')
 wind_speed=$(echo $response | jq -r '.wind.speed') 
 wind_speed_kmh=$(echo "$wind_speed * 3.6" | awk '{printf "%.1f", $0}')
-output="Thoi tiet nhiet do: $temp °C. Do am: $humidity % . Gio: $wind_speed_kmh km/h"
+output="Thoi tiet nhiet do: $temp °C. Do am: $humidity % . Gio: $wind_speed $wind_speed_kmh km/h"
 
 echo $output | iconv -f UTF-8 -t ASCII//TRANSLIT > thoitiet.txt
